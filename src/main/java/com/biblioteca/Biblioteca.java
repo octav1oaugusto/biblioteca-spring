82eb56824fa1632bd058450396821f0d14762b9e
octav1oaugusto <octavioaugusto.cs@hotmail.com>
2022-06-06 23:31:40 -0300

    Adicionar configuração base de dados

---
+++ src/main/java/com/biblioteca/Biblioteca.java
@@ -0,0 +1,21 @@
+package com.biblioteca;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class Biblioteca {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Biblioteca.class, args);
+	}
+
+}
